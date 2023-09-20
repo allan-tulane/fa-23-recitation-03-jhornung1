b05f0229d0338e339f4682f63471a2faa3078ab4
jhornung1 <126175822+jhornung1@users.noreply.github.com>
2023-09-20 16:41:45 +0000

    Final Version

--- main.py
+++ main.py
@@ -12,6 +12,24 @@ class BinaryNumber:
         
     def __repr__(self):
         return('decimal=%d binary=%s' % (self.decimal_val, ''.join(self.binary_vec)))
+
+    def __add__(self, other):
+      max_len = max(len(self.binary_vec), len(other.binary_vec))
+      self_vec = ['0'] * (max_len - len(self.binary_vec)) + self.binary_vec
+      other_vec = ['0'] * (max_len - len(other.binary_vec)) + other.binary_vec
+
+      carry = 0
+      result_vec = []
+
+      for i in range(max_len - 1, -1, -1):
+        bit_sum = int(self_vec[i]) + int(other_vec[i]) + carry
+        result_vec.insert(0, str(bit_sum % 2))
+        carry = bit_sum // 2
+
+      if carry:
+        result_vec.insert(0, '1')
+
+      return BinaryNumber(int(''.join(result_vec), 2))
     
 
 ## Implement multiplication functions here. Note that you will have to
@@ -49,20 +67,51 @@ def quadratic_multiply(x, y):
     return _quadratic_multiply(x,y).decimal_val
 
 def _quadratic_multiply(x, y):
-    ### TODO
-    pass
-    ###
+  xvec = x.binary_vec
+  yvec = y.binary_vec
+
+  # Pad xvec and yvec with leading 0s to make them the same length
+  max_len = max(len(xvec), len(yvec))
+  xvec = ['0'] * (max_len - len(xvec)) + xvec
+  yvec = ['0'] * (max_len - len(yvec)) + yvec
+
+  # Base case: If both x and y are <= 1, return their product
+  if x.decimal_val <= 1 or y.decimal_val <= 1:
+    return BinaryNumber(x.decimal_val * y.decimal_val)
+
+  # Split xvec and yvec into two halves
+  n = len(xvec)
+  n_half = n // 2
+
+  x_left = binary2int(xvec[:n_half])
+  x_right = binary2int(xvec[n_half:])
+  y_left = binary2int(yvec[:n_half])
+  y_right = binary2int(yvec[n_half:])
+
+  # Recursive calls to compute the sub-products
+  xy_left = _quadratic_multiply(x_left, y_left)
+  xy_right = _quadratic_multiply(x_right, y_right)
+  xy_cross = (_quadratic_multiply(x_left, y_right) + _quadratic_multiply(x_right, y_left))
+
+  # Perform the necessary bit shifts
+  result = (bit_shift(xy_left, 2 * (n - n_half)) +
+            bit_shift(xy_cross, (n - n_half)) +
+            xy_right)
+
+  return result
 
 
-    
-    
 def test_quadratic_multiply(x, y, f):
-    start = time.time()
-    # multiply two numbers x, y using function f
-    
-    return (time.time() - start)*1000
+  start = time.time()
+  # multiply two numbers x, y using function f
+  result = f(x, y)
+  
+  return (time.time() - start)*1000
 
 
-    
-    
+## Testing the test function
+x = BinaryNumber(100)
+y = BinaryNumber(100)
 
+print(test_quadratic_multiply(x, y, quadratic_multiply))
+      
--- test_main.py
+++ test_main.py
@@ -4,4 +4,7 @@ from main import *
 
 ## Feel free to add your own tests here.
 def test_multiply():
-    assert quadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+  assert quadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+  assert quadratic_multiply(BinaryNumber(3), BinaryNumber(4)) == 3*4
+  assert quadratic_multiply(BinaryNumber(5), BinaryNumber(4)) == 5*4
+  assert quadratic_multiply(BinaryNumber(100), BinaryNumber(100)) == 100*100
